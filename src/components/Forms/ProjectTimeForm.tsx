d14e737c841c1e339cf79c7d8bb94e4bf54e7413
Erik <erik.p.schmitt@gmail.com>
2025-04-28 23:11:55 +0200

    Fixed displaying times in the right timezone.

--- src/components/Forms/ProjectTimeForm.tsx
+++ src/components/Forms/ProjectTimeForm.tsx
@@ -1,102 +1,89 @@
-import { Project } from "../../hooks/useProjects";
+import { Project } from '../../hooks/useProjects';
 
 interface Props {
-  newProject: Project;
-  updateProject: (project: Project) => void;
+	newProject: Project;
+	updateProject: (project: Project) => void;
 }
 const formatDateForInput = (date: Date): string => {
-  const isoString = date.toISOString();
-  return isoString.slice(0, 16);
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	return `${year}-${month}-${day}T${hours}:${minutes}`;
 };
 
 const ProjectTimeForm = ({ newProject, updateProject }: Props) => {
-  const handleStartTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputDate = new Date(e.target.value);
-    const utcDate = new Date(
-      Date.UTC(
-        inputDate.getFullYear(),
-        inputDate.getMonth(),
-        inputDate.getDate(),
-        inputDate.getHours(),
-        inputDate.getMinutes()
-      )
-    );
-    updateProject({
-      ...newProject,
-      config: {
-        ...newProject.config,
-        votingStartDayAndTime: utcDate,
-      },
-    });
-  };
+	const handleStartTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const inputDate = new Date(e.target.value);
+		updateProject({
+			...newProject,
+			config: {
+				...newProject.config,
+				votingStartDayAndTime: inputDate,
+			},
+		});
+	};
 
-  const handleEndTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const inputDate = new Date(e.target.value);
-    const utcDate = new Date(
-      Date.UTC(
-        inputDate.getFullYear(),
-        inputDate.getMonth(),
-        inputDate.getDate(),
-        inputDate.getHours(),
-        inputDate.getMinutes()
-      )
-    );
-    updateProject({
-      ...newProject,
-      config: {
-        ...newProject.config,
-        votingEndDayAndTime: utcDate,
-      },
-    });
-  };
+	const handleEndTimeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const inputDate = new Date(e.target.value);
+		updateProject({
+			...newProject,
+			config: {
+				...newProject.config,
+				votingEndDayAndTime: inputDate,
+			},
+		});
+	};
+	const handleTimeCheckboxChange = () => {
+		updateProject({
+			...newProject,
+			config: {
+				...newProject.config,
+				useTime: !newProject.config.useTime,
+			},
+		});
+	};
 
-  const handleTimeCheckboxChange = () => {
-    updateProject({
-      ...newProject,
-      config: {
-        ...newProject.config,
-        useTime: !newProject.config.useTime,
-      },
-    });
-  };
-
-  return (
-    <div>
-      <div>
-        <label htmlFor="useTimeSpan">Use Time Span:</label>
-        <input
-          type="checkbox"
-          id="useTimeSpan"
-          checked={newProject.config.useTime}
-          onChange={handleTimeCheckboxChange}
-        />
-      </div>
-      {newProject.config.useTime && (
-        <div>
-          <div>
-            <label htmlFor="startTime">Start Time:</label>
-            <input
-              type="datetime-local"
-              id="startTime"
-              value={formatDateForInput(
-                newProject.config.votingStartDayAndTime
-              )}
-              onChange={handleStartTimeChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="endTime">End Time:</label>
-            <input
-              type="datetime-local"
-              id="endTime"
-              value={formatDateForInput(newProject.config.votingEndDayAndTime)}
-              onChange={handleEndTimeChange}
-            />
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div>
+			<div>
+				<label htmlFor='useTimeSpan'>Use Time Span:</label>
+				<input
+					type='checkbox'
+					id='useTimeSpan'
+					checked={newProject.config.useTime}
+					onChange={handleTimeCheckboxChange}
+				/>
+			</div>
+			{newProject.config.useTime && (
+				<div>
+					<div>
+						<label htmlFor='startTime'>Start Time:</label>
+						<input
+							type='datetime-local'
+							id='startTime'
+							value={formatDateForInput(
+								newProject.config.votingStartDayAndTime
+							)}
+							onChange={handleStartTimeChange}
+						/>
+					</div>
+					<div>
+						<label htmlFor='endTime'>End Time:</label>
+						<input
+							type='datetime-local'
+							id='endTime'
+							value={formatDateForInput(
+								newProject.config.votingEndDayAndTime
+							)}
+							onChange={handleEndTimeChange}
+						/>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default ProjectTimeForm;
